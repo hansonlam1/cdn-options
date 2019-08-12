64efa7529334036af064723289d0ed0399495654
hansonlam1 <hansonlam1@gmail.com>
2019-08-11 21:03:58 -0700

    starting off

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # main part of the program
+from bs4 import BeautifulSoup
+import requests
+import time
+import pandas as pd
+import tickerlist
+from datetime import datetime, timedelta
+
+# for a specific list of tickers, retrieve option quotes
+tickers = tickerlist.TICKERLIST
+mindaystoexpiry = 10
+maxdaystoexpiry = 60
+percentstrikerange = 0.05
+maindf = pd.DataFrame()
+
+# loop through and grab the quotes
+# calculate the annualized return on a best case scenario
